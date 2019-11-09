697ef60e853c15c39205d4b791d2209dc581d052
3wdian0 <13599488868@163.com>
2019-11-09 12:46:14 +0800

    先完成《算法导论》钢条切割，剪绳子OK，自底向上方法，模板一样

--- Dynamic/14-剪绳子/maxProductAfterCutting_dynamicSolution.cc
+++ Dynamic/14-剪绳子/maxProductAfterCutting_dynamicSolution.cc
@@ -1,29 +1,53 @@
 #include<iostream>
 #include<vector>
+#include<limits.h>
+
+#define max(x,y) (x>y)?x:y;
 
 using namespace std;
 
 /*
  *
+ * 绳子长度为n
+ * 剪成m段：k[0] k[1] k[2] . . . k[m-1]
+ * (n>1, m>1)
+ * k[0]*k[1]*k[2]*...*k[m-1]
+ *
  * 长度为：1
  * 长度为：2
  * 长度为：3
  *
- * 绳子
- * 长度为n：- - - - - -
- * 剪成m段：k[0] k[1] k[2] . . . k[m]
- * (n>1, m>1)
  */
 
-int maxProductAfterCutting_dynamicSolution(int n, int m){
+int maxProductAfterCutting_dynamicSolution(int n){
     vector<int> num ;
-    for(int i = 0; i<=n; i++){
-        num.push_back(0);
+    for(int i = 0; i<=5; i++){
+        num.push_back(1);
     }
-    num[1] = 1;
-    num[2] = 2;
-    return 0;
+    /*
+     * num[1] = 1;
+     * num[2] = 2;
+     * num[3] = 3;
+     * num[4] = 4;
+     * num[5] = 2*3;
+     */
+    for(int j=1; j<=n; j++){
+        int q = INT_MIN;
+        for(int i=1;i<=j;i++){
+            q = max(q, i*num[j-i]);
+        }
+        num[j] = q;
+    }
+
+    return num[n];
 }
 int main(int argc, char * argv []) {
+    int i;
+    i=1;cout<<i<<"->"<<maxProductAfterCutting_dynamicSolution(i)<<endl;
+    i=2;cout<<i<<"->"<<maxProductAfterCutting_dynamicSolution(i)<<endl;
+    i=3;cout<<i<<"->"<<maxProductAfterCutting_dynamicSolution(i)<<endl;
+    i=4;cout<<i<<"->"<<maxProductAfterCutting_dynamicSolution(i)<<endl;
+    i=5;cout<<i<<"->"<<maxProductAfterCutting_dynamicSolution(i)<<endl;
+    i=8;cout<<i<<"->"<<maxProductAfterCutting_dynamicSolution(i)<<endl;
     return 0;
 }
