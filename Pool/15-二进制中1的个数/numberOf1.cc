99a9b5c34568959ac1f5922681084b5e88ad42bb
3wdian0 <13599488868@163.com>
2019-11-13 22:27:34 +0800

    15-二进制中1的个数，法一：暴力解

--- Pool/15-二进制中1的个数/numberOf1.cc
+++ Pool/15-二进制中1的个数/numberOf1.cc
@@ -1,8 +1,48 @@
 #include<iostream>
 #include<vector>
+#include<string>
+#include<stack>
 
 using namespace std;
 
+/*
+ * 18/2=9···0
+ *  9/2=4···1
+ *  4/2=2···0
+ *  2/2=1···0 
+ *  1/2=0···1
+ */
+
+void binary(int x){
+    stack<int> s;
+    while(x/2!=0){
+        s.push(x%2);
+        x = x/2;
+    }
+    if(x%2!=0)
+        s.push(1);
+    while(!s.empty()){
+        int i = s.top();
+        s.pop();
+        cout<<i;
+    }
+    cout<<endl;
+} 
+
+int method1(int x){
+    int n = 0;
+    while(x/2!=0){
+        if(x%2==1)
+            n++;
+        x /=2;
+    }
+    if(x%2!=0)
+        n++;
+    return n;
+}
+
 int main(int argc, char *argv[]){
+    binary(18);
+    cout<<method1(18)<<endl;
     return 0;
 }
