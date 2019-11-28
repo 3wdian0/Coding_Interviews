c3172d37417693b6e9aa2c31808b7cccd1939dc7
3wdian0 <13599488868@163.com>
2019-11-28 09:57:18 +0800

    12-矩阵中的路径：看答案完成

--- BackTrack/12-矩阵中的路径/haspath.cc
+++ BackTrack/12-矩阵中的路径/haspath.cc
@@ -13,8 +13,9 @@ void _print(vector<vector<char> > matrix){
         cout<<endl;
     }
 }
+bool hasPathCore(vector<vector<char> > &matrix, int i, int j, int &length, vector<vector<bool> > visited, string &str);
 
-bool hasPath(vector<vector<char> > matrix){
+bool hasPath(vector<vector<char> > matrix, string str){
     // visited 标记
     vector<vector<bool> > visited;
     for(int i=0; i<3; i++){
@@ -28,15 +29,34 @@ bool hasPath(vector<vector<char> > matrix){
     int length = 0;
     for(int i=0; i<matrix.size(); i++){
         for(int j=0; j<matrix[0].size(); j++){
-            //hasPathCore();
+            if(hasPathCore(matrix,i,j,length,visited,str)){
+                cout<<"找到"<<str<<endl;
+                return true;
+            }
         }
     }
 
-   return true;
+    cout<<"没找到"<<str<<endl;
+    return false;
 }
 
-bool hasPathCore(vector<vector<char> > &matrix, int i, int j, int &length, vector<vector<bool> > visitd){
-    return true;
+bool hasPathCore(vector<vector<char> > &matrix, int i, int j, int &length, vector<vector<bool> > visited, string &str){
+    if(str[length]=='\0'){
+        // 停止条件
+        return true;
+    }
+
+    bool tag = false;
+    if(i<matrix.size() && j<matrix[0].size() && i>=0 && j>=0 && str[length]==matrix[i][j] && visited[i][j]==false){
+        visited[i][j] = true;
+        length += 1;
+        tag = (hasPathCore(matrix,i+1,j,length,visited,str)||hasPathCore(matrix,i,j+1,length,visited,str)||hasPathCore(matrix,i-1,j,length,visited,str)||hasPathCore(matrix,i,j-1,length,visited,str));
+        if(tag==false){
+            visited[i][j] = false;
+            length -= 1;
+        }
+    }
+    return tag;
 }
 int main(int arc, char *argv[]){
     vector<vector<char> > matrix;
@@ -45,8 +65,10 @@ int main(int arc, char *argv[]){
     c.push_back('c');c.push_back('f');c.push_back('c');c.push_back('s'); matrix.push_back(c);c.clear();
     c.push_back('j');c.push_back('d');c.push_back('e');c.push_back('h'); matrix.push_back(c);c.clear();
     _print(matrix);
-    string s1 = "bfce";
-    string s2 = "abfb";
+    string s = "bfce";
+    hasPath(matrix,s);
+    s = "abfb";
+    hasPath(matrix,s);
     return 0;
 
 }
